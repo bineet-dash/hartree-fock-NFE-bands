1754a4f9b638388004ddb03e5cf497c025d3082e
Bineet Dash <bineetdash@gmail.com>
2017-12-10 20:22:13 +0530

    just before final commit

--- screened-hf-bloch.cpp
+++ screened-hf-bloch.cpp
@@ -141,67 +141,75 @@ int main()
     eigenspectrum.clear();
   }
 
+
   spectrum* nonint_arr = new spectrum [N];
   for(int i=0; i<N; i++) nonint_arr[i] = arr[i];
+
+  Ef = nonint_arr[0].at(0).first; //Fermi Energy
+  lambda = sqrt(3*electron_density/(2*Ef));
+  cout << "lambda = " << lambda << endl;
+
 
   spectrum int_eigenspectrum;
   char choice_for_result = 'n';
   int master_loop = 1;
   spectrum* int_arr = new spectrum [N];
 
-  for( ; ; )
+  for(int m=0; m<2; m++)
   {
-    std::cout << "Loop:" << master_loop << "\n===================================\n" << '\n';
-    int m = 0;
-
-    Ef = arr[0].at(0).first; //Fermi Energy
-    lambda = sqrt(3*electron_density/(2*Ef));
-    cout << "lambda = " << lambda << endl;
-
-    for(int kindex=0; kindex<N; kindex++)
+    for( ; ; )
     {
-      double k = (2*M_PI*kindex)/(N*a)-M_PI/a;
-      for(int i=0; i<unitcell_point.size(); i++)
+      std::cout << "Loop:" << master_loop << "\n===================================\n" << '\n';
+      for(int kindex=0; kindex<N; kindex++)
       {
-        int j = (i==unitcell_point.size()-1)? 0 : i+1;
-        H(i,j)= cd(-1/(2*dx*dx), -k/(2*dx));
-        H(j,i)= cd(-1/(2*dx*dx), k/(2*dx));
-        H(i,i)= cd(1/(dx*dx)+ pow(k,2)/2+ V(unitcell_point(i)), 0)+2*vh(m,kindex,i)-vhf(m,kindex,i);
+        double k = (2*M_PI*kindex)/(N*a)-M_PI/a;
+        for(int i=0; i<unitcell_point.size(); i++)
+        {
+          int j = (i==unitcell_point.size()-1)? 0 : i+1;
+          H(i,j)= cd(-1/(2*dx*dx), -k/(2*dx));
+          H(j,i)= cd(-1/(2*dx*dx), k/(2*dx));
+          H(i,i)= cd(1/(dx*dx)+ pow(k,2)/2+ V(unitcell_point(i)), 0)+2*vh(m,kindex,i)-vhf(m,kindex,i);
+        }
+
+        diagonalize(H,v,eigenvectors);
+
+        for(int i=0; i<unitcell_point.size(); i++)  int_eigenspectrum.push_back(make_pair(v[i].real(), eigenvectors.col(i)));
+        sort(int_eigenspectrum.begin(),int_eigenspectrum.end(),compare);
+        int_eigenspectrum.resize(N);
+        int_arr[kindex] = int_eigenspectrum;
+        int_eigenspectrum.clear();
       }
 
-      diagonalize(H,v,eigenvectors);
+      VectorXd diff(N);
+      for(int i=0; i<N; i++)  diff(i) = abs(int_arr[i].at(m).first-arr[i].at(m).first);
 
-      for(int i=0; i<unitcell_point.size(); i++)  int_eigenspectrum.push_back(make_pair(v[i].real(), eigenvectors.col(i)));
-      sort(int_eigenspectrum.begin(),int_eigenspectrum.end(),compare);
-      int_eigenspectrum.resize(N);
-      int_arr[kindex] = int_eigenspectrum;
-      int_eigenspectrum.clear();
+      if(choice_for_result=='y')
+      {
+        for(int i=0; i<N; i++)
+        cout << i << " " << arr[i].at(m).first << " " << int_arr[i].at(m).first << " " << diff(i) << endl;
+        cout << endl;
+      }
+
+      for(int i=0; i<N; i++) arr[i] = int_arr[i];
+
+      cout << "Max diff= " << diff.maxCoeff() << endl << endl;
+      double tolerance = (m==0)?1e-3:0.003;
+      if(diff.maxCoeff()<tolerance)  break;
+      master_loop++;
     }
 
-    VectorXd diff(N);
-    for(int i=0; i<N; i++)  diff(i) = abs(int_arr[i].at(m).first-arr[i].at(m).first);
-
-    if(choice_for_result=='y')
-    {
-      for(int i=0; i<N; i++)
-      cout << i << " " << arr[i].at(m).first << " " << int_arr[i].at(m).first << " " << diff(i) << endl;
-      cout << endl;
-    }
-
-    for(int i=0; i<N; i++) arr[i] = int_arr[i];
-
-    cout << "Max diff= " << diff.maxCoeff() << endl << endl;
-    if(diff.maxCoeff()<1e-3)  break;
-    master_loop++;
   }
 
 
+  ofstream dataout("hf_data_screened.txt");
+  for(int i=0; i<N; i++)
   {
-    int m=0;
-    ofstream dataout("screened_hf_data.txt");
-    for(int i=0; i<N; i++)
-      dataout << (2*M_PI*i)/(N*a)-M_PI/a << " " << nonint_arr[i].at(m).first << " " << arr[i].at(m).first << endl;
+    dataout << (2*M_PI*i)/(N*a)-M_PI/a << " ";
+    for(int m=0; m<2; m++)
+      dataout << nonint_arr[i].at(m).first << " " << arr[i].at(m).first << " ";
+    dataout << endl;
   }
+
 
 
   return 0;
